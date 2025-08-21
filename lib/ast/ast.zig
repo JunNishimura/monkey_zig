09c11681a9d837aaa9ba8c8b27992257e8e0fd59
Jun Nishimura <n.junjun0303@gmail.com>
2025-08-22 00:29:22 +0900

    feat: add string method to node interface

--- lib/ast/ast.zig
+++ lib/ast/ast.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
+const testing = std.testing;
 const Token = @import("token").Token;
 
 const Node = struct {
     ptr: *anyopaque,
     token_literal_fn: *const fn (ptr: *anyopaque) []const u8,
+    string_fn: *const fn (ptr: *anyopaque) anyerror![]const u8,
+    deinit_fn: *const fn (ptr: *anyopaque, allocator: std.mem.Allocator) void,
 
     pub fn init(ptr: anytype) Node {
         const T = @TypeOf(ptr);
@@ -14,23 +17,42 @@ const Node = struct {
                 const self: T = @ptrCast(@alignCast(pointer));
                 return ptr_info.pointer.child.token_literal(self);
             }
+
+            pub fn string(pointer: *anyopaque) ![]const u8 {
+                const self: T = @ptrCast(@alignCast(pointer));
+                return ptr_info.pointer.child.string(self);
+            }
+
+            pub fn deinit(pointer: *anyopaque, allocator: std.mem.Allocator) void {
+                const self: T = @ptrCast(@alignCast(pointer));
+                ptr_info.pointer.child.deinit(self, allocator);
+            }
         };
 
         return .{
             .ptr = ptr,
             .token_literal_fn = gen.token_literal,
+            .string_fn = gen.string,
+            .deinit_fn = gen.deinit,
         };
     }
 
     fn token_literal(self: Node) []const u8 {
         return self.token_literal_fn(self.ptr);
     }
+
+    fn string(self: Node) ![]const u8 {
+        return self.string_fn(self.ptr);
+    }
+
+    fn deinit(self: Node, allocator: std.mem.Allocator) void {
+        self.deinit_fn(self.ptr, allocator);
+    }
 };
 
 pub const Statement = struct {
     ptr: *anyopaque,
     statement_node_fn: *const fn (ptr: *anyopaque) void,
-    deinit_fn: *const fn (ptr: *anyopaque, allocator: std.mem.Allocator) void,
     node: Node,
 
     pub fn init(ptr: anytype) Statement {
@@ -42,17 +64,11 @@ pub const Statement = struct {
                 const self: T = @ptrCast(@alignCast(pointer));
                 return ptr_info.pointer.child.statement_node(self);
             }
-
-            pub fn deinit(pointer: *anyopaque, allocator: std.mem.Allocator) void {
-                const self: T = @ptrCast(@alignCast(pointer));
-                ptr_info.pointer.child.deinit(self, allocator);
-            }
         };
 
         return .{
             .ptr = ptr,
             .statement_node_fn = gen.statement_node,
-            .deinit_fn = gen.deinit,
             .node = Node.init(ptr),
         };
     }
@@ -62,17 +78,22 @@ pub const Statement = struct {
     }
 
     pub fn deinit(self: Statement, allocator: std.mem.Allocator) void {
-        self.deinit_fn(self.ptr, allocator);
+        self.node.deinit(allocator);
     }
 
     pub fn token_literal(self: Statement) []const u8 {
         return self.node.token_literal();
+    }
+
+    pub fn string(self: Statement) ![]const u8 {
+        return self.node.string();
     }
 };
 
 const Expression = struct {
     ptr: *anyopaque,
     expression_node_fn: *const fn (ptr: *anyopaque) void,
+    node: Node,
 
     pub fn init(ptr: anytype) Expression {
         const T = @TypeOf(ptr);
@@ -88,6 +109,7 @@ const Expression = struct {
         return .{
             .ptr = ptr,
             .expression_node_fn = gen.expression_node,
+            .node = Node.init(ptr),
         };
     }
 
@@ -98,10 +120,19 @@ const Expression = struct {
     fn token_literal(self: Expression) []const u8 {
         return self.node.token_literal();
     }
+
+    fn deinit(self: Expression, allocator: std.mem.Allocator) void {
+        self.node.deinit(allocator);
+    }
+
+    fn string(self: Expression) ![]const u8 {
+        return self.node.string();
+    }
 };
 
 pub const Program = struct {
     statements: std.ArrayList(Statement),
+    str_list: std.ArrayList(u8),
 
     fn token_literal(self: *Program) []const u8 {
         if (self.statements.len > 0) {
@@ -110,11 +141,23 @@ pub const Program = struct {
         return "";
     }
 
+    pub fn string(self: *Program) ![]const u8 {
+        for (self.statements.items) |stmt| {
+            const stmt_str = try stmt.string();
+            try self.str_list.appendSlice(stmt_str);
+        }
+
+        const out: []const u8 = self.str_list.items;
+        return out;
+    }
+
     pub fn init(allocator: std.mem.Allocator) !*Program {
         const p = try allocator.create(Program);
         const stmts = std.ArrayList(Statement).init(allocator);
+        const str_list = std.ArrayList(u8).init(allocator);
         p.* = .{
             .statements = stmts,
+            .str_list = str_list,
         };
         return p;
     }
@@ -124,6 +167,7 @@ pub const Program = struct {
             stmt.deinit(allocator);
         }
         self.statements.deinit();
+        self.str_list.deinit();
         allocator.destroy(self);
     }
 };
@@ -132,11 +176,33 @@ pub const LetStatement = struct {
     token: Token,
     name: ?*Identifier,
     value: ?Expression,
+    str_list: std.ArrayList(u8),
 
     fn token_literal(self: *LetStatement) []const u8 {
         return self.token.literal;
     }
+
     fn statement_node(_: *LetStatement) void {}
+
+    fn string(self: *LetStatement) ![]const u8 {
+        try self.str_list.appendSlice(self.token_literal());
+        try self.str_list.appendSlice(" ");
+        if (self.name) |n| {
+            const name_str = try n.string();
+            try self.str_list.appendSlice(name_str);
+        } else {
+            try self.str_list.appendSlice("<no name>");
+        }
+        try self.str_list.appendSlice(" = ");
+        if (self.value) |v| {
+            const value_str = try v.string();
+            try self.str_list.appendSlice(value_str);
+        } else {
+            try self.str_list.appendSlice("<no value>");
+        }
+        try self.str_list.appendSlice(";");
+        return self.str_list.items;
+    }
 
     pub fn statement(self: *LetStatement) Statement {
         return Statement.init(self);
@@ -148,6 +214,7 @@ pub const LetStatement = struct {
             .token = token,
             .name = null,
             .value = null,
+            .str_list = std.ArrayList(u8).init(allocator),
         };
         return stmt;
     }
@@ -156,6 +223,7 @@ pub const LetStatement = struct {
         if (self.name) |n| {
             allocator.destroy(n);
         }
+        self.str_list.deinit();
         allocator.destroy(self);
     }
 };
@@ -163,11 +231,26 @@ pub const LetStatement = struct {
 pub const ReturnStatement = struct {
     token: Token,
     return_value: ?Expression,
+    str_list: std.ArrayList(u8),
 
     pub fn token_literal(self: *ReturnStatement) []const u8 {
         return self.token.literal;
     }
+
     fn statement_node(_: *ReturnStatement) void {}
+
+    fn string(self: *ReturnStatement) ![]const u8 {
+        try self.str_list.appendSlice(self.token_literal());
+        try self.str_list.appendSlice(" ");
+        if (self.return_value) |rv| {
+            const return_value_str = try rv.string();
+            try self.str_list.appendSlice(return_value_str);
+        } else {
+            try self.str_list.appendSlice("<no return value>");
+        }
+        try self.str_list.appendSlice(";");
+        return self.str_list.items;
+    }
 
     pub fn statement(self: *ReturnStatement) Statement {
         return Statement.init(self);
@@ -178,18 +261,20 @@ pub const ReturnStatement = struct {
         stmt.* = .{
             .token = token,
             .return_value = null,
+            .str_list = std.ArrayList(u8).init(allocator),
         };
         return stmt;
     }
 
     pub fn deinit(self: *ReturnStatement, allocator: std.mem.Allocator) void {
+        self.str_list.deinit();
         allocator.destroy(self);
     }
 };
 
 pub const ExpressionStatement = struct {
     token: Token,
-    expression: Expression,
+    expression: ?Expression,
 
     fn statement_node(_: *ExpressionStatement) void {}
     fn token_literal(self: *ExpressionStatement) []const u8 {
@@ -198,16 +283,22 @@ pub const ExpressionStatement = struct {
     fn deinit(self: *ExpressionStatement, allocator: std.mem.Allocator) void {
         allocator.destroy(self);
     }
+    fn string(self: *ExpressionStatement) ![]const u8 {
+        if (self.expression) |e| {
+            return e.string();
+        }
+        return "";
+    }
 
     fn statement(self: *ExpressionStatement) Statement {
         return Statement.init(self);
     }
 
-    pub fn init(allocator: std.mem.Allocator, token: Token, expression: Expression) !*ExpressionStatement {
+    pub fn init(allocator: std.mem.Allocator, token: Token) !*ExpressionStatement {
         const stmt = try allocator.create(ExpressionStatement);
         stmt.* = .{
             .token = token,
-            .expression = expression,
+            .expression = null,
         };
         return stmt;
     }
@@ -221,6 +312,12 @@ pub const Identifier = struct {
         return self.token.literal;
     }
     fn expression_node(_: *Identifier) void {}
+    pub fn deinit(self: *Identifier, allocator: std.mem.Allocator) void {
+        allocator.destroy(self);
+    }
+    fn string(self: *Identifier) ![]const u8 {
+        return self.value;
+    }
 
     pub fn init(allocator: std.mem.Allocator, token: Token, value: []const u8) !*Identifier {
         const ident = try allocator.create(Identifier);
@@ -235,3 +332,21 @@ pub const Identifier = struct {
         return Expression.init(self);
     }
 };
+
+test "test string" {
+    const allocator = testing.allocator;
+
+    const program = try Program.init(allocator);
+    defer program.deinit(allocator);
+
+    const let_stmt = try LetStatement.init(allocator, Token{ .type = .Let, .literal = "let" });
+
+    let_stmt.name = try Identifier.init(allocator, Token{ .type = .Ident, .literal = "myVar" }, "myVar");
+    const value = try Identifier.init(allocator, Token{ .type = .Ident, .literal = "anotherVar" }, "anotherVar");
+    defer value.deinit(allocator);
+    let_stmt.value = value.expression();
+    try program.statements.append(let_stmt.statement());
+
+    const out = try program.string();
+    try testing.expect(std.mem.eql(u8, out, "let myVar = anotherVar;"));
+}
