b357eb0b8f1d71ab8cdcfe2b9b7de03a64a08504
Jun Nishimura <n.junjun0303@gmail.com>
2025-09-06 21:49:42 +0900

    feat: define return object

--- lib/object/object.zig
+++ lib/object/object.zig
@@ -4,18 +4,21 @@ const ObjectType = enum {
     integer,
     boolean,
     null,
+    return_obj,
 };
 
 pub const Object = union(ObjectType) {
     integer: i64,
     boolean: bool,
     null: void,
+    return_obj: Object,
 
     pub fn getType(self: Object) ObjectType {
         return switch (self) {
             .integer => ObjectType.integer,
             .boolean => ObjectType.boolean,
             .null => ObjectType.null,
+            .return_obj => ObjectType.return_obj,
         };
     }
 
@@ -24,6 +27,7 @@ pub const Object = union(ObjectType) {
             .integer => try std.fmt.allocPrint(allocator, "{d}", .{self.integer}),
             .boolean => try std.fmt.allocPrint(allocator, "{any}", .{self.boolean}),
             .null => "null",
+            .return_obj => try self.return_obj.inspect(allocator),
         };
     }
 };
